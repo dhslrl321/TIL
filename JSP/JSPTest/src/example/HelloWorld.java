1841c7fb799f95d6a1cdb6c1c393fb2cfd95ca13
dhslrl321 <dhslrl321@gmail.com>
2020-01-18 14:51:12 +0900

    jsp 실습 환경 setting

---
+++ JSP/JSPTest/src/example/HelloWorld.java
@@ -0,0 +1,7 @@
+package example;public class HelloWorld {
+  public String sayHelloWorldFrom(String from) {
+    String result = "Hello, world, from " + from;
+    System.out.println(result);
+    return result;
+  }
+}
