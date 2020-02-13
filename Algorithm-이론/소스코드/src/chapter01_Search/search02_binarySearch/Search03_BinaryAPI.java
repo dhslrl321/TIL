65514fbfbb5f17201457c5ecba2df30c5910f932
dhslrl321 <dhslrl321@gmail.com>
2020-02-13 13:45:14 +0900

    Binary Api in action

--- Algorithm-이론/소스코드/src/chapter01_Search/search02_binarySearch/Search03_BinaryAPI.java
+++ Algorithm-이론/소스코드/src/chapter01_Search/search02_binarySearch/Search03_BinaryAPI.java
@@ -1,4 +1,15 @@
 package chapter01_Search.search02_binarySearch;
 
+import java.util.Arrays;
+
 public class Search03_BinaryAPI {
+    public static void main(String[] args) {
+        int[] arr = {9, 12, 13, 14, 15, 1, 2, 3, 4, 6, 7, 8};
+        Arrays.sort(arr);
+        int ans = Arrays.binarySearch(arr, 9);
+
+        System.out.println(ans);
+    }
 }
+
+
