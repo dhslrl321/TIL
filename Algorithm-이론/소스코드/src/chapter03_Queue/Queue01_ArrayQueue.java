418898482604aa2a30206f619db32e748189f67e
dhslrl321 <dhslrl321@gmail.com>
2020-02-16 14:03:08 +0900

    queue

--- Algorithm-이론/소스코드/src/chapter03_Queue/Queue01_ArrayQueue.java
+++ Algorithm-이론/소스코드/src/chapter03_Queue/Queue01_ArrayQueue.java
@@ -15,15 +15,31 @@ class IntQueue{
 
     public IntQueue(int capacity){
         max = capacity;
+        rear = -1;
+        front = 0;
         queue = new int[max];
     }
-
-    void insert(){
-
+    boolean empty(){
+        return (front == (rear+1));
     }
 
-    void remove(){
+    boolean full(){
+        return (rear >= max-1);
+    }
 
+    // rear로 데이터를 추가
+    void add(int x){
+        if(full()) throw new ArrayIndexOutOfBoundsException();
+        queue[++rear] = x;
+    }
+    // front는 데이터를 뺼 때만 사용
+    int remove(){
+        if (empty()) throw new ArrayIndexOutOfBoundsException();
+        return queue[front++];
+    }
+
+    int peek(){
+        return queue[front];
     }
 
 }
--- Algorithm-이론/소스코드/src/chapter03_Queue/Queue03_QueueAPI.java
+++ Algorithm-이론/소스코드/src/chapter03_Queue/Queue03_QueueAPI.java
@@ -1,4 +1,20 @@
 package chapter03_Queue;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Queue03_QueueAPI {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.add(10);
+        queue.add(20);
+
+        System.out.println(queue);
+        System.out.println(queue.peek());
+        System.out.println(queue);
+        System.out.println(queue.remove());
+
+    }
 }
