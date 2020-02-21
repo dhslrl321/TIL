c83ac78e0bde0d0b6101c515ee8849358162ade5
dhslrl321 <dhslrl321@gmail.com>
2020-02-21 13:16:14 +0900

    123

--- Algorithm-이론/소스코드/src/chapter03_Queue/Queue03_QueueAPI.java
+++ Algorithm-이론/소스코드/src/chapter03_Queue/Queue03_QueueAPI.java
@@ -8,13 +8,5 @@ public class Queue03_QueueAPI {
     public static void main(String[] args) {
         Queue<Integer> queue = new LinkedList<>();
 
-        queue.add(10);
-        queue.add(20);
-
-        System.out.println(queue);
-        System.out.println(queue.peek());
-        System.out.println(queue);
-        System.out.println(queue.remove());
-
     }
 }
--- Algorithm-이론/소스코드/src/chapter04_Sort/Prob02_SortingNum2.java
+++
@@ -1,7 +0,0 @@
-package chapter04_Sort;
-
-public class Prob02_SortingNum2 {
-    public static void main(String[] args) {
-
-    }
-}
