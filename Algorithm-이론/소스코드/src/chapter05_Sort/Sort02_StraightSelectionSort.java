babcb437946aa2d50f52aa919649d50993228768
dhslrl321 <dhslrl321@gmail.com>
2020-02-21 17:28:45 +0900

    123

--- Algorithm-이론/소스코드/src/chapter05_Sort/Sort02_StraightSelectionSort.java
+++ Algorithm-이론/소스코드/src/chapter05_Sort/Sort02_StraightSelectionSort.java
@@ -2,26 +2,27 @@ package chapter05_Sort;
 
 import java.util.Scanner;
 
-public class Sort01_BubbleSort {
+public class Sort02_StraightSelectionSort {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         int n = input.nextInt();
-        int temp;
         int[] arr = new int[n];
-
+        int temp, index = 0, min;
         for (int i = 0; i < n; i++) {
             arr[i] = input.nextInt();
         }
-
+        min = arr[0];
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length - 1; j++) {
-                if (arr[j] > arr[j + 1]){
-                    temp = arr[j+1];
-                    arr[j+1] = arr[j];
-                    arr[j] = temp;
+            for (int j = i; j < arr.length; j++) {
+                if(min > arr[j]) {
+                    min = arr[j];
+                    index = i;
                 }
             }
+            temp = arr[i];
+            arr[i] = min;
+            arr[index] = temp;
         }
         for(int i : arr) System.out.println(i);
     }
