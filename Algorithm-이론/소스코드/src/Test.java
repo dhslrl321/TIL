3bef98a6b601844c34164683692ae52e11df49a5
dhslrl321 <dhslrl321@gmail.com>
2020-02-24 14:10:10 +0900

    123

--- Algorithm-이론/소스코드/src/Test.java
+++ Algorithm-이론/소스코드/src/Test.java
@@ -1,2 +1,20 @@
+import java.util.*;
+
 public class Test {
+    public static void main(String[] args) {
+
+        String[] arr = {"Hello", "world", "I'm", "Groot", "Say", "My", "Name"};
+
+        Arrays.sort(arr, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o2.length() - o1.length();
+            }
+        });
+
+        for(String iter : arr) System.out.println(iter);
+
+    }
 }
+
+
