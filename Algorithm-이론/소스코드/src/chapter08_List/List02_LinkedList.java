4503f0a2ccd6d1227e7fe37ded5e3c78b63368a3
dhslrl321 <dhslrl321@gmail.com>
2020-02-28 00:27:29 +0900

    LinkedList 중단

--- Algorithm-이론/소스코드/src/chapter08_List/List02_LinkedList.java
+++ Algorithm-이론/소스코드/src/chapter08_List/List02_LinkedList.java
@@ -2,6 +2,18 @@ package chapter08_List;
 
 public class List02_LinkedList {
     public static void main(String[] args) {
-        
+
+
     }
 }
+
+class LinkedList_Node<E>{
+    private E data;
+    private LinkedList_Node<E> next;
+
+    LinkedList_Node(E data, LinkedList_Node<E> next){
+        this.data = data;
+        this.next = next;
+    }
+}
+
