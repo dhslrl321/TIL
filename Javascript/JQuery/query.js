e1a2a290344981b5faab13765818fd255ad2ac07
dhslrl321 <dhslrl321@gmail.com>
2020-04-24 02:49:42 +0900

    123

--- Javascript/JQuery/query.js
+++ Javascript/JQuery/query.js
@@ -29,8 +29,14 @@ jquersy_function = () => {
 
     $var_name.html("변경할 문자열"); // html에 쓰기 == innerHTML();
 
+    $("#chkbox_id").is(":checked") ;    //- id가 동일한 체크박스에 대하여 선택되어 있는지 확인 : true / false 반환
+
 }
 
 /*
     제이쿼리 테이블 : https://zetawiki.com/wiki/JQuery_%ED%85%8C%EC%9D%B4%EB%B8%94_%ED%96%89_%EC%B6%94%EA%B0%80/%EC%82%AD%EC%A0%9C
+*/
+
+/*
+    체크박스 제어하기 : https://fruitdev.tistory.com/143 
 */
