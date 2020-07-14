731483ebd952c834d9948e139c3703bc5abd558f
dhslrl321 <dhslrl321@gmail.com>
2020-07-14 14:23:36 +0900

    html update

--- Javascript/NodeJs/Node01_dataType.mjs
+++ Javascript/NodeJs/Node01_dataType.mjs
@@ -1,15 +1,6 @@
-function car(brand, wheel){
-    this.brand = brand;
-    this.wheel = wheel;
+function clicked(){
+    window.alert("button has clicked");
 }
 
-var santafe = new car("hyundai", 4);
-
-console.log(santafe);
-var a = new String("asdf");
-console.log(a);
-
-let date = new Date();
-console.log(date);
 
 
