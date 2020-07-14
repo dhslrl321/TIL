823b3555720df50b1ebd0892c12311606cea4dac
dhslrl321 <dhslrl321@gmail.com>
2020-07-14 18:19:26 +0900

    update

--- Javascript/React-project/my-first-app/src/App.js
+++ Javascript/React-project/my-first-app/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import TodoListTemplate from './components/TodoListTemplate';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        App
-      </div>
+      <TodoListTemplate>
+        템플릿 완성
+      </TodoListTemplate>
     );
   }
 }
