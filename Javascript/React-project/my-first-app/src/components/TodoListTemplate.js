c8c4cd1180699b09a5a954ccb9e4a2119ae72de3
dhslrl321 <dhslrl321@gmail.com>
2020-07-14 21:39:54 +0900

    update readme

--- Javascript/React-project/my-first-app/src/components/TodoListTemplate.js
+++ Javascript/React-project/my-first-app/src/components/TodoListTemplate.js
@@ -5,7 +5,7 @@ const TodoListTemplate = ({form, children}) => {
   return (
     <main className="todo-list-template">
       <div className="title">
-        오늘 할 일
+        To do
       </div>
       <section className="form-wrapper">
         {form}
