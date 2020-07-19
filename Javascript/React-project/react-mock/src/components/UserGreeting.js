6507a163d100b0ba25c9a09a550b12dc74f1b90e
dhslrl321 <dhslrl321@gmail.com>
2020-07-19 22:38:22 +0900

    asdf

--- Javascript/React-project/react-mock/src/components/UserGreeting.js
+++ Javascript/React-project/react-mock/src/components/UserGreeting.js
@@ -4,12 +4,20 @@ class UserGreeting extends Component {
 
     constructor(props){
         super(props);
-        this.state {
-            names : this.props.firstName + this.props.lastName;
+        this.state = {
+            name : "",
+            names : this.props.firstName + this.props.lastName
         }
     }
 
+    toFullSencentence(){
+        this.setState(this.state.names + "입니다");
+    }
+
     render() {
+        this.setState((state, props) => {
+            name : this.state.names + "입니다"
+        });
         return (
             <section>
                 <div>{this.state.names}</div>
