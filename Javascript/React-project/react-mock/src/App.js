e9b29be75744184ca10757099b10f164d88eceb9
dhslrl321 <dhslrl321@gmail.com>
2020-07-21 17:08:42 +0900

    router

--- Javascript/React-project/react-mock/src/App.js
+++ Javascript/React-project/react-mock/src/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Router } from 'react-router-dom';
 
 class App extends Component {
   render(){
     return (
-      <div></div>
+      <Router>
+        <div>
+          <Router path="/home">
+            Home Page
+          </Router>
+
+          <Router path="/user">
+            User Page
+          </Router>
+        </div>
+      </Router>
     );
   }
 }
