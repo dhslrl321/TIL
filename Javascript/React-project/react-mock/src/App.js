72ec23fe62848f8ad16717b69cba775af2ba2ea8
dhslrl321 <dhslrl321@gmail.com>
2020-07-22 21:04:05 +0900

    asdf

--- Javascript/React-project/react-mock/src/App.js
+++ Javascript/React-project/react-mock/src/App.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
 import PhoneForm from './components/PhoneForm';
+import Greeting from './components/Greeting';
 
 class App extends Component {
 
+  state = {
+    name: '',
+    phone: ''
+  }
+
   handleCreate = (data) => {
-    console.log(data);
+    console.log(data)
+    this.setState({
+      name: data.name,
+      phone: data.phone
+    })
   }
   
   render(){
     return ( 
       <div>
         <PhoneForm onCreate={this.handleCreate} />
+        <Greeting name={this.state.name} phone={this.state.phone}></Greeting>
       </div>
     );
   }
