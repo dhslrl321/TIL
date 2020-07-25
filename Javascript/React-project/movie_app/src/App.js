59b08af1c0fba5892671428af93b141344c545b4
dhslrl321 <dhslrl321@gmail.com>
2020-07-25 14:55:42 +0900

    asdf

--- Javascript/React-project/movie_app/src/App.js
+++ Javascript/React-project/movie_app/src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import axios from 'axios';
 
 
 class App extends React.Component {
 
   state = {
-    isLoading: true
+    isLoading: true,
+    movies: []
   };
 
+  getMovies = async () => {
+    const { data:
+      { data:
+        { movies }
+      }
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    this.setState({ isLoading: false });
+  }
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false })
-    }, 3000);
+    this.getMovies();
   }
 
   render() {
