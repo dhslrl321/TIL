4f3d3014a81e73abbefd43ddc255ccd5d2d17679
dhslrl321 <dhslrl321@gmail.com>
2020-07-30 13:37:24 +0900

    asdf

--- Javascript/React-project/react-mock/src/components/topMenu/forum/Forum.js
+++ Javascript/React-project/react-mock/src/components/topMenu/forum/Forum.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 
 class Forum extends Component {
+
+  state = {
+    data: ''
+  }
+  getTestJson = () => {
+    const { data } = await axios.get("http://localhost:8080/api/board").then
+    this.setState({
+      data: data
+    })
+  }
+
+  componentDidMount() {
+    this.getTestJson();
+  }
   render() {
+    const { data } = this.state;
     return (
-      <div> this is forum</div>
+      <div> {data.data}</div>
     );
   }
 }
