e375a5338550430307838a9716c6eb65218672dd
dhslrl321 <dhslrl321@gmail.com>
2020-07-30 20:53:27 +0900

    asdf

--- Javascript/React-project/tester/src/App.js
+++ Javascript/React-project/tester/src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import TOC from './component/TOC';
+import Content from './component/Content';
+import Subject from './component/Subject';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mode: "welcome",
+      selected_content_id: 2,
+      welcome: { title: "welcome", content: "Hello React" },
+      subject: { title: "web", sub: "World wide web" },
+      content: [
+        { id: 1, title: "html", desc: "html is for information" },
+        { id: 2, title: "css", desc: "html is for information" },
+        { id: 3, title: "javascript", desc: "html is for information" }
+      ]
+    }
+  }
+
+  render() {
+
+    let _title, _desc = null;
+
+    if (this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if (this.state.mode === 'read') {
+      let i = 0;
+      while (i < this.state.content.length) {
+        let data = this.state.content[i];
+        if (data.id === this.state.selected_content_id) {
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i = i + 1
+      }
+    }
+    return (
+      <div>
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}
+          onChangePage={function () {
+            console.log("변경 전 : ", this.state.mode)
+            this.setState({
+              mode: "welcome"
+            })
+            console.log("변경 후 : ", this.state.mode);
+          }.bind(this)} />
+
+        <TOC
+          data={this.state.content}
+          onChangePage={function (id) {
+            this.setState({ mode: "read", selected_content_id: Number(id) })
+          }.bind(this)} />
+
+        <Content title={_title} desc={_desc} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ Javascript/React-project/tester/src/component/TOC.js
@@ -0,0 +1,31 @@
+
+import React, { Component } from 'react';
+class TOC extends Component {
+
+  render() {
+    let lists = [];
+    let data = this.props.data;
+    let i = 0;
+    while (i < data.length) {
+      lists.push(
+        <li key={data[i].id}>
+          <a
+            href={"/content/" + data[i].id}
+            data-id={data[i].id}
+            onClick={function (e) {
+              e.preventDefault();
+              this.props.onChangePage(e.target.dataset.id);
+            }.bind(this)}
+          >{data[i].title}</a>
+        </li>)
+      i = i + 1;
+    }
+    return (
+      <div>
+        {lists}
+      </div>
+    );
+  }
+}
+
+export default TOC;
