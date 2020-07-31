006c15f3959f31c1fd146837927bd705835624a9
dhslrl321 <dhslrl321@gmail.com>
2020-07-31 11:48:22 +0900

    asdf

--- HTML/Clone-Coding/solved.ac/section/index.js
+++ HTML/Clone-Coding/solved.ac/section/index.js
@@ -1,9 +1,25 @@
+let car = {
+    wheel: 4,
+    engine: 2,
+    color: {
+        sheet: 'brown',
+        handle: 'black',
+        outlook: 'white',
+    }
+}
 
-let btn = document.getElementById("root");
+let bike = JSON.parse(JSON.stringify(car));
 
-alert("hello world");
+console.log(car)
+console.log(bike)
+
+console.log(car === bike)
+
+car.color.handle = 'red';
+
+console.log(car)
+console.log(bike)
 
 
-btn.innerHTML("good");
 
-btn.appendChild("Child Component");
+
