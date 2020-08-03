df5191edb52b04ed973e0d2b244c481f0910a513
dhslrl321 <dhslrl321@gmail.com>
2020-08-03 20:25:01 +0900

    router

--- Javascript/React-project/egoing-clone/src/components/top/TopTemplate.js
+++ Javascript/React-project/egoing-clone/src/components/top/TopTemplate.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 class TopTemplate extends Component {
 
   render() {
     return (
       <div>
-        hello
+
       </div>
     );
 
