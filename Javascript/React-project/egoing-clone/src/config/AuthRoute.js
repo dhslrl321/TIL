227854e9a5a1446e4838278291bad1ad978cea0f
dhslrl321 <dhslrl321@gmail.com>
2020-08-23 11:09:43 +0900

    auth

--- Javascript/React-project/egoing-clone/src/config/AuthRoute.js
+++ Javascript/React-project/egoing-clone/src/config/AuthRoute.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
 
-const AuthRoute = () => {
-  <Route {...rest} render={} />
+const AuthRoute = ({ authenticated, component: Component, render, ...rest }) => {
+  <Route {...rest} render={
+    (props) => authenticated ? (
+      render ? (render(props)) : (
+        <Component {...props} />
+      )
+    )
+  } />
 }
 
 export default AuthRoute;
