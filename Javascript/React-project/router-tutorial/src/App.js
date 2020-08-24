bf31c3f4ac026dfd2244e7124b47e663889054a5
dhslrl321 <dhslrl321@gmail.com>
2020-08-24 20:20:23 +0900

    createupadte

--- Javascript/React-project/router-tutorial/src/App.js
+++ Javascript/React-project/router-tutorial/src/App.js
@@ -1,12 +1,64 @@
-import React from "react";
-import Counter from "./components/Counter";
-import InputSample from "./components/InputSample";
+import React, { useRef, useState } from "react";
+import CreateUser from "./components/CreateUser";
+import UserList from "./components/UserList";
 
 const App = () => {
 
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: ""
+  })
+
+  const { username, email } = inputs;
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  }
+
+  const users = [
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    }
+  ];
+
+  const nextId = useRef(4);
+
+  const onCreate = () => {
+
+    setInputs({
+      username: "",
+      email: ""
+    })
+    nextId.current += 1;
+  }
   return (
     <div>
-      <InputSample />
+      <br></br>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      /><br></br>
+
+      <UserList users={users} />
     </div>
   );
 }
--- Javascript/React-project/router-tutorial/src/components/UserList.js
+++ Javascript/React-project/router-tutorial/src/components/UserList.js
@@ -8,20 +8,15 @@ const User = ({ user }) => {
   )
 }
 
-const UserList = () => {
-  const users = [
-    { id: 1, username: "dhslrl321", email: "dhslrl321@gmail.com" },
-    { id: 2, username: "heo0125", email: "heo015@gmail.com" },
-    { id: 3, username: "park102", email: "ffuiw2@gmail.com" }
-  ];
+const UserList = ({ users }) => {
 
   return (
     <div>
-      {users.map(user => {
-        <User user={user} />
-      })}
+      {users.map(user => (
+        <User key={user.id} user={user} />
+      ))}
     </div>
   )
 }
 
-export default { User, UserList }
+export default UserList;
