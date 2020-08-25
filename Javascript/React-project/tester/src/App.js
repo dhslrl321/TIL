dc77fae6d98ffdeb27fa49ee277d0e14bf92a432
dhslrl321 <dhslrl321@gmail.com>
2020-08-25 19:05:03 +0900

    imp

--- Javascript/React-project/tester/src/App.js
+++ Javascript/React-project/tester/src/App.js
@@ -1,23 +1,11 @@
 import React, { Component } from 'react';
-import TOC from './component/TOC';
-import ReadContent from './component/ReadContent';
-import Subject from './component/Subject';
-import Control from './component/Control';
-import CreateContent from './component/CreateContent';
 
-class App extends Component {
-
-  render() {
-    third = () => {
-      console.log("asdf")
-    }
-  }
-
-  return(
-    { third }
+const App = () => {
+  return (
+    <div>
+      hello
+    </div>
   );
-
-
 }
 
 export default App;
