4f32910088eb29b90a3047fb8e4fdf67e9965d08
dhslrl321 <dhslrl321@gmail.com>
2020-09-03 10:43:14 +0900

    appp

--- Javascript/React-project/react-mock/src/App.js
+++ Javascript/React-project/react-mock/src/App.js
@@ -1,15 +1,24 @@
-import React, { Component } from 'react';
-import './App.css';
-import MenuTemplate from './components/MenuTemplate';
-class App extends Component {
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
 
-  render() {
-    return (
-      <div>
-        <MenuTemplate></MenuTemplate>
-      </div>
-    );
+const App = () => {
+  const [dataString, setDataString] = useState("");
+
+  const connectServer = async () => {
+    await axios.get("http://localhost:8080/test").then(response => (
+      setDataString({
+        dataString: response
+      })
+    ))
   }
+
+  useEffect(() => {
+    connectServer();
+  })
+
+  return (
+    <div>{dataString}</div>
+  );
 }
 
 
