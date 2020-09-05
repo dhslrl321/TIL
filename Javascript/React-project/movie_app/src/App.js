5786fbaa19e190257056576cfa2549137d30426b
dhslrl321 <dhslrl321@gmail.com>
2020-09-06 00:37:53 +0900

    clear proj

--- Javascript/React-project/movie_app/src/App.js
+++ Javascript/React-project/movie_app/src/App.js
@@ -1,46 +1,11 @@
 import React from 'react';
-import axios from 'axios';
-import Movie from './Movie';
-import './App.css';
 
-class App extends React.Component {
-
-  state = {
-    isLoading: true,
-    movies: []
-  };
-
-  getMovies = async () => {
-    const { data: {
-      data: { movies }
-    } } = await axios.get("https://yts-proxy.now.sh/list_movies.json/sort_by=rating");
-    this.setState({ movies, isLoading: false });
-  }
-
-  componentDidMount() {
-    this.getMovies();
-  }
-
-  render() {
-    const { isLoading, movies } = this.state;
-    return (
-      <section className="container">
-        {isLoading ?
-          <div className="loader">
-            <span className="loader__text">Loading</span>
-          </div> : movies.map(movie => {
-            return <Movie
-              id={movie.id}
-              year={movie.year}
-              title={movie.title}
-              summary={movie.summary}
-              poster={movie.medium_cover_image}
-              genres={movie.genres} />
-          })}
-
-      </section>
-    );
-  }
+const App = () => {
+  return (
+    <div>
+      hello
+    </div>
+  );
 }
 
 export default App;
