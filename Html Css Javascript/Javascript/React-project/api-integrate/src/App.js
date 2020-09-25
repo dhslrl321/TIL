89d1d144750c67d3b199332ab67b802da46edbe3
dhslrl321 <dhslrl321@gmail.com>
2020-09-25 22:55:12 +0900

    asdf'

--- Html Css Javascript/Javascript/React-project/api-integrate/src/App.js
+++ Html Css Javascript/Javascript/React-project/api-integrate/src/App.js
@@ -1,10 +1,46 @@
-import React from 'react';
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import axios from "axois";
 
 function App() {
+
+  const [users, setUsers] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        setError(null);
+        setUsers(null);
+
+        setLoading(true);
+
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+
+        setUsers(response.data);
+      } catch (e) {
+        setError(e);
+      }
+
+      setLoading(false);
+    };
+
+    fetchUsers();
+  }, [])
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>You just got Error</div>;
+  if (!users) return null;
+
   return (
     <div>
-      asdf
+      {users.map(user => (
+        <li key={user.id}>
+          {user.username} ({user.name})
+        </li>
+      ))}
     </div>
   );
 }
