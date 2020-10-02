090e7628bd031f185d1a4a9cc90d7e5dbe5ba0cf
dhslrl321 <dhslrl321@gmail.com>
2020-10-02 22:04:37 +0900

    selector

--- Html Css Javascript/Javascript/React-project/tester/src/App.js
+++ Html Css Javascript/Javascript/React-project/tester/src/App.js
@@ -1,6 +1,17 @@
-import React, { useState, useRef } from "react";
+import React from "react";
 
 const App = () => {
+
+  const name = "react";
+  const fn = () => true
+
+  const message = `hello ${fn}`;
+
+  return (
+    <div>
+      {message}
+    </div>
+  );
 }
 
 export default App;
