f533312fbd68227f6ec1b435777a3756a299cefe
dhslrl321 <dhslrl321@gmail.com>
2020-10-06 21:52:41 +0900

    pu

--- Html Css Javascript/Javascript/React-project/nomadcoder-styled-components/src/App.js
+++ Html Css Javascript/Javascript/React-project/nomadcoder-styled-components/src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    padding: 0;
+    margin: 0;
+  }
+`
 
 class App extends Component {
   render() {
     return (
       <Container>
-        <Button>Hello</Button>
-        <Button danger />
+        <GlobalStyle></GlobalStyle>
+        <Button> Hello</Button>
+        <Button danger>Hello</Button>
+        <Anchor href="http://google.com">goto google</Anchor>
       </Container>
     );
   };
@@ -31,7 +40,11 @@ const Button = styled.button`
   &:focus{
     outline: none;
   }
-  background-color: ${props => (props.danger ? "yellow" : "green")};
+  background-color: ${props => (props.danger ? "yellow" : "green")}
+`;
+
+const Anchor = styled(Button.withComponent("a"))`
+  text-decoration: none;
 `;
 
 export default App;
