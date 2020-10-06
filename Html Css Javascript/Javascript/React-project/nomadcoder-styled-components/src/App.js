0e7449fc6c58514e76210a726ed03620d25775f2
dhslrl321 <dhslrl321@gmail.com>
2020-10-07 04:24:38 +0900

    rebuild

--- Html Css Javascript/Javascript/React-project/nomadcoder-styled-components/src/App.js
+++ Html Css Javascript/Javascript/React-project/nomadcoder-styled-components/src/App.js
@@ -2,15 +2,40 @@ import React from 'react';
 import { BrowserRouter as Router } from "react-router-dom";
 import { Header } from './components/Header';
 import { Routes } from './components/Routes';
+import styled from "styled-components";
+import GlobalStyles from "./components/GlobalStyles"
 
 export const App = () => {
   return (
-    <Router>
-      <Header />
-      <br></br>
-      <Routes />
-    </Router>
+    <>
+
+      <RootContainer>
+        <TextField>Box1</TextField>
+        <TextField>Box2</TextField>
+      </RootContainer>
+    </>
   );
 }
 
+const RootContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const TextField = styled.span`
+  display: flex;
+  width: 150px;
+  height: 150px;
+  background-color: wheat;
+  justify-content: center;
+  align-items: center;
+`;
+
 export default App;
+
+
+// <Router>
+// <Header />
+// <br></br>
+// <Routes />
+//</Router>
