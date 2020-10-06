0611764c2235ef6e4f50fb6e07772de19a3d995f
dhslrl321 <dhslrl321@gmail.com>
2020-10-06 23:06:21 +0900

    reamde' gitpu

--- Html Css Javascript/Javascript/React-project/nomadcoder/src/Components/GlobalStyles.js
+++ Html Css Javascript/Javascript/React-project/nomadcoder/src/Components/GlobalStyles.js
@@ -14,6 +14,8 @@ const GlobalStyles = createGlobalStyle`
         font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
         font-size: 14px;
         background-color: rgba(20, 20, 20, 1);
+        color: white;
+        padding-top: 50px;
     }
 `;
 
