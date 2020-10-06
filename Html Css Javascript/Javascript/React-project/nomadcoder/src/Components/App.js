92ba5c52791c4a7fada08d4bc6887d1c91b875a2
dhslrl321 <dhslrl321@gmail.com>
2020-10-06 22:12:47 +0900

    adsf

--- Html Css Javascript/Javascript/React-project/nomadcoder/src/Components/App.js
+++ Html Css Javascript/Javascript/React-project/nomadcoder/src/Components/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import Router from "Components/Router";
-import Header from "Components/Header";
+import GlobalStyles from "Components/GlobalStyles";
 
 class App extends Component {
   render() {
     return (
       <>
-        <Header />
+        <GlobalStyles />
         <Router />
       </>
     );
--- Html Css Javascript/Javascript/React-project/nomadcoder/src/Components/Header.js
+++ Html Css Javascript/Javascript/React-project/nomadcoder/src/Components/Header.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const List = styled.ul`
+  display: flex;
+  &:hover {
+    background-color: blue;
+  }
+`;
+
+const Item = styled.li``;
+
+const SLink = styled(Link)``;
 
 export default () => (<header>
-  <ul>
-    <li>
-      <a href="/">Movies</a>
-    </li>
-    <li>
-      <a href="/tv">TV</a>
-    </li>
-    <li>
-      <a href="/search">Search</a>
-    </li>
-  </ul>
+  <List>
+    <Item>
+      <SLink to="/">Movies</SLink>
+    </Item>
+    <Item>
+      <SLink to="/tv">TV</SLink>
+    </Item>
+    <Item>
+      <SLink to="/search">Search</SLink>
+    </Item>
+  </List>
 </header>)
