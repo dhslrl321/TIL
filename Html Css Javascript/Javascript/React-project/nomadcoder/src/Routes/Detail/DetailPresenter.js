213b15d2f869ccf2f90e41193ae73413da96817d
dhslrl321 <dhslrl321@gmail.com>
2020-10-09 12:53:19 +0900

    presenter

--- Html Css Javascript/Javascript/React-project/nomadcoder/src/Routes/Detail/DetailPresenter.js
+++ Html Css Javascript/Javascript/React-project/nomadcoder/src/Routes/Detail/DetailPresenter.js
@@ -1,1 +1,15 @@
-export default () => "Detail";
+import React from 'react';
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const DetailPresenter = ({ topRated, popular, airingToday, loading, error }) => null;
+
+TVPresenter.propTypes = {
+    topRated: PropTypes.array,
+    popular: PropTypes.array,
+    airingToday: PropTypes.array,
+    loading: PropTypes.bool.isRequired,
+    error: PropTypes.string
+}
+
+export default DetailPresenter;
--- Html Css Javascript/Javascript/React-project/nomadcoder/src/Routes/Search/SearchPresenter.js
+++ Html Css Javascript/Javascript/React-project/nomadcoder/src/Routes/Search/SearchPresenter.js
@@ -1,1 +1,22 @@
-export default () => "Search";
+import React from 'react';
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const SearchPresenter = ({
+    movieResults,
+    tvResults,
+    searchTerm,
+    loading,
+    error,
+    handleSubmit }) => null;
+
+TVPresenter.propTypes = {
+    movieResults: PropTypes.array,
+    tvResults: PropTypes.array,
+    searchTerm: PropTypes.string,
+    loading: PropTypes.bool.isRequired,
+    error: PropTypes.string,
+    handleSubmit: PropTypes.func.isRequired
+}
+
+export default SearchPresenter;
