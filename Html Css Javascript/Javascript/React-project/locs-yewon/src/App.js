6c9b12bda22f7a46f24728c899e288fb50486983
dhslrl321 <dhslrl321@gmail.com>
2020-10-16 21:03:29 +0900

    com

--- Html Css Javascript/Javascript/React-project/locs-yewon/src/App.js
+++ Html Css Javascript/Javascript/React-project/locs-yewon/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import Header from "./Header";
-import styled from "styled-components";
+import Test from './Test';
 
 function App() {
   return (
     <div>
-      hello
+      <Test />
     </div>
   );
 }
