e083cf83a663a20c3eb2ed3cebb1d3a00c4f8b32
dhslrl321 <dhslrl321@gmail.com>
2020-10-19 08:36:20 +0900

    config

--- Html Css Javascript/Javascript/React-project/locs-wonik/src/api.js
+++ Html Css Javascript/Javascript/React-project/locs-wonik/src/api.js
@@ -1,14 +1,27 @@
 import axios from "axios";
+import { Common } from "./config";
 
 const api = axios.create({
   baseURL: "http://localhost:8080/api",
+  params: {
+    transactionTIme: Date.now(),
+    resultCode: "200 OK",
+    description: "Client",
+    data: {}
+  }
 });
 
 export const TestApi = {
   getTest: () => api.get("test/get"),
-  postTest: (data) => api.post("test/post", data),
+  postTest: () => api.post("test/post", data),
   putTest: () => api.put("test/put"),
   delete: () => api.delete("test/delete")
+}
+
+export const UserApi = {
+  findUser: () => api.get("user"),
+  createUser: () => api.post("user", data),
+
 }
 
 export default api;
---
+++ Html Css Javascript/Javascript/React-project/locs-wonik/src/config.js
@@ -0,0 +1,8 @@
+const Common = {
+  transactionTIme: Date.now(),
+  resultCode: "200 OK",
+  description: "Client",
+  data: {}
+}
+
+export default Common;
