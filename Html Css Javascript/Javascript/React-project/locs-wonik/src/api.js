940120435ab890a0f9c818ca9ea20318255d9791
dhslrl321 <dhslrl321@gmail.com>
2020-10-20 17:24:48 +0900

    commit

--- Html Css Javascript/Javascript/React-project/locs-wonik/src/api.js
+++ Html Css Javascript/Javascript/React-project/locs-wonik/src/api.js
@@ -2,11 +2,6 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://localhost:8080/api",
-  params: {
-    transactionTime: Date.now(),
-    resultCode: "200 OK",
-    description: "Client"
-  }
 });
 
 export const UserApi = {
