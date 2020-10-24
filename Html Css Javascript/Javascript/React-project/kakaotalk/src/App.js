f65bd995469e14bb6459f0abb71836b50c846707
dhslrl321 <dhslrl321@gmail.com>
2020-10-25 08:35:08 +0900

    comm

--- Html Css Javascript/Javascript/React-project/kakaotalk/src/App.js
+++ Html Css Javascript/Javascript/React-project/kakaotalk/src/App.js
@@ -1,56 +1,11 @@
-import React, { useReducer } from 'react';
-import { createGlobalStyle } from "styled-components";
-import TodoTemplate from './components/TodoTemplate';
-import TodoHead from "./components/TodoHead";
-import TodoList from "./components/TodoList";
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    background-color: #e9ecef;
-  }
-`;
-
-function reducer(state, action) {
-  switch (action.type) {
-    case "INCREMENT":
-      return state + 1;
-    case "DECREMENT":
-      return state - 1;
-    default:
-      return state;
-  }
-}
-
-function Counter() {
-  const [number, dispatch] = useReducer(reducer, 0);
-
-  const onIncrease = () => {
-    dispatch({
-      type: "INCREMENT"
-    });
-  }
-
-  const onDecrease = () => {
-    dispatch({
-      type: "DECREMENT"
-    });
-  }
-
-  return (
-    <div>
-      <h1>{number}</h1>
-      <button onClick={onIncrease}> + 1</button>
-      <button onClick={onDecrease}> - 1</button>
-    </div>
-  )
-}
-
+import React, { useRef, useState, useMemo, useCallback, useReducer } from 'react';
+import translations from "./translations";
+import Lang from './context';
 const App = () => {
   return (
-    <>
-      <GlobalStyle />
-      <Counter></Counter>
-    </>
+    <Lang defaultLang="en" translations={translations}>
+
+    </Lang>
   );
 }
 
