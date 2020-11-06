5f2ae8f05fad9caed9067d60c892fa8ec48ff6ea
dhslrl321 <dhslrl321@gmail.com>
2020-11-06 18:01:58 +0900

    commit

--- Html Css Javascript/Javascript/React-project/brian-web-site/src/App.js
+++ Html Css Javascript/Javascript/React-project/brian-web-site/src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import "./App.css";
+import Sidebar from "./components/Sidebar/index";
 
 function App() {
   return (
     <Router>
+      <Sidebar />
       <Navbar />
     </Router>
   );
--- Html Css Javascript/Javascript/React-project/brian-web-site/src/components/Sidebar/index.js
+++ Html Css Javascript/Javascript/React-project/brian-web-site/src/components/Sidebar/index.js
@@ -1,9 +1,37 @@
 import React from 'react'
-
+import {
+  SidebarContainer, SideBtnWrap, SidebarLink,
+  SidebarRoute, SidebarMenu, SidebarWrapper,
+  Icon, CloseIcon
+} from "./styles";
 const Sidebar = () => {
   return (
-    <>
-    </>
+    <SidebarContainer>
+      <Icon>
+        <CloseIcon />
+      </Icon>
+      <SidebarWrapper>
+        <SidebarMenu>
+          <SidebarLink to="about">
+            about
+          </SidebarLink>
+          <SidebarLink to="discover">
+            discover
+          </SidebarLink>
+          <SidebarLink to="services">
+            services
+          </SidebarLink>
+          <SidebarLink to="signup">
+            signup
+          </SidebarLink>
+        </SidebarMenu>
+      </SidebarWrapper>
+      <SideBtnWrap>
+        <SidebarRoute to="/signin">
+          Signin
+        </SidebarRoute>
+      </SideBtnWrap>
+    </SidebarContainer>
   )
 }
 
