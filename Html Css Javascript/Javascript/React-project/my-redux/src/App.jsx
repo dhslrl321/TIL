fb9e796fc7d4d1fdd435cb9a26634916ad6caa71
dhslrl321 <dhslrl321@gmail.com>
2020-12-04 21:41:38 +0900

    commit

--- Html Css Javascript/Javascript/React-project/my-redux/src/App.jsx
+++ Html Css Javascript/Javascript/React-project/my-redux/src/App.jsx
@@ -1,16 +1,45 @@
 import "./App.css";
+import { connect } from "react-redux";
+import { actionCreator } from "./store";
+import React, { useState } from "react";
 
-function App() {
+function App({ state, onButtonClick }) {
+  const [inputs, setInputs] = useState({
+    name: "",
+    email: "",
+  });
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  const onClick = () => {};
+
+  const { name, email } = inputs;
   return (
     <div>
       <h1>이름을 등록해보세요!</h1>
-      <input type="text" />
-      <button>등록하기</button>
+      <input type="text" name="name" value={name} onChange={onChange} />
+      <input type="text" name="email" value={email} onChange={onChange} />
+      <button onClick={onButtonClick}>등록하기</button>
       <ul>
         <li>sdaf</li>
       </ul>
     </div>
   );
 }
-
-export default App;
+function mapStateToProps(state) {
+  console.log(state);
+  return { state };
+}
+function mapDispatchToProps(dispatch) {
+  return {
+    onButtonClick: (name, email) =>
+      dispatch(actionCreator.addUser(name, email)),
+  };
+}
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- Html Css Javascript/Javascript/React-project/my-redux/src/store.js
+++ Html Css Javascript/Javascript/React-project/my-redux/src/store.js
@@ -1,17 +1,27 @@
 import { createStore } from "redux";
 
-const ADD_TODO = "ADD_TODO";
-const DELETE_TODO = "DELETE_TODO";
+const ADD_USER = "ADD_USER";
+
+const addUser = (name, email) => {
+  store.dispatch({ type: ADD_USER, user: { name, email } });
+}
+
 
 const reducer = (state = [], action) => {
   switch (action.type) {
-    case (ADD_TODO): {
-
-    }
-    case (DELETE_TODO): {
-
+    case (ADD_USER): {
+      const { name = "hello", email = "no" } = action.user;
+      return [{ name, email, id: Date.now() }, ...state]
     }
     default:
       return state;
   }
 }
+
+export const actionCreator = {
+  addUser: (name, email) => addUser(name, email)
+}
+
+const store = createStore(reducer);
+
+export default store;
