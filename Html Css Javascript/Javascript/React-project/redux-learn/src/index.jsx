686add4032f360521d72f7bcdf63361d71391c8c
dhslrl321 <dhslrl321@gmail.com>
2020-12-04 13:56:27 +0900

    commit

--- Html Css Javascript/Javascript/React-project/redux-learn/src/index.jsx
+++ Html Css Javascript/Javascript/React-project/redux-learn/src/index.jsx
@@ -1,33 +1,5 @@
-import { createStore } from "redux";
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./components/App";
 
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.getElementById("span");
-
-const reducer = (count = 0, action) => {
-  if (action.type === "ADD") {
-    return count + 12;
-  } else if (action.type === "MINUS") {
-    return count - 1;
-  } else return count;
-};
-const store = createStore(reducer);
-
-number.innerText = store.getState();
-
-const updateText = () => {
-  number.innerText = count;
-};
-
-const handleAdd = () => {
-  store.dispatch({ type: "ADD" });
-  updateText();
-};
-
-const handleMinus = () => {
-  store.dispatch({ type: "MINUS" });
-  updateText();
-};
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+ReactDom.render(<App></App>, document.getElementById("root"));
