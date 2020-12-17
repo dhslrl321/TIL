f679d6f9a3d9ff5ab0d4165e7e4f6d0f7afff37d
dhslrl321 <dhslrl321@gmail.com>
2020-12-17 22:28:12 +0900

    commit

--- Html Css Javascript/Javascript/React-project/my-redux/src/container/PostListContainer.js
+++ Html Css Javascript/Javascript/React-project/my-redux/src/container/PostListContainer.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import PostList from '../components/PostList';
+import { getPosts } from "../modules/posts";
 
 const PostListContainer = () => {
   const { data, loading, error } = useSelector(state => state.posts.posts);
--- Html Css Javascript/Javascript/React-project/my-redux/src/modules/index.js
+++ Html Css Javascript/Javascript/React-project/my-redux/src/modules/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
 import counter from "./counter";
+import posts from "./posts";
 const rootReducer = combineReducers({ counter, posts })
 
 export default rootReducer;
