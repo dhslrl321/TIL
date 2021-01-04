12da46e1f2c977ba392c2a5db31473e0dd82c971
dhslrl321 <dhslrl321@gmail.com>
2021-01-04 20:13:58 +0900

    commit

--- Html Css Javascript/Javascript/React-project/next-styled/pages/index.js
+++ Html Css Javascript/Javascript/React-project/next-styled/pages/index.js
@@ -1,19 +1,5 @@
 import styled from "styled-components";
 
-const Container = styled.div`
-  height: 100vh;
-  width: 100vw;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Button = styled.button`
-  height: 100px;
-  background: wheat;
-  border-radius: 2px;
-  font-size: 2rem;
-`;
 export default function Home() {
   return (
     <Container>
