74899511a3b19b7e30403fceb81b9e5e77c878a3
dhslrl321 <dhslrl321@gmail.com>
2021-01-19 10:24:34 +0900

    commit

--- Html Css Javascript/Javascript/React-project/real/components/atoms/Image/style.js
+++ Html Css Javascript/Javascript/React-project/real/components/atoms/Image/style.js
@@ -1,19 +1,12 @@
 import styled, { css } from 'styled-components';
-import { theme } from 'styled-tools';
-
-const defaultStyle = css`
-  width: 100px;
-`;
 
 export const Picture = styled.img`
-  ${defaultStyle}
   width: 120px;
   border-radius: 60px;
   background: powderblue;
 `;
 
 export const Icon = styled.img`
-${defaultStyle}
   width: 50%;
   height: auto;
 `;
