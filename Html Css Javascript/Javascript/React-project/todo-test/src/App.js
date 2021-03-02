fb80609d544818075b3cf84feb88dcaef8a20a12
dhslrl321 <dhslrl321@gmail.com>
2021-03-02 11:55:32 +0900

    commit

--- Html Css Javascript/Javascript/React-project/todo-test/src/App.js
+++ Html Css Javascript/Javascript/React-project/todo-test/src/App.js
@@ -1,16 +1,5 @@
-import { useEffect } from "react";
-
-
-
-// Example of use:
-setCookie('user', 'John', { secure: true, 'max-age': 3600 });
-
 function App() {
-  useEffect(() => {
-    setCookie('JSESSIONID_DORMWEB', "hello.WAS3_servlet_engine6", {});
-    setCookie('WMONID', 'VE3D9hw7a4R', {});
-    // window.location.href = "https://dormweb.pcu.ac.kr/common/login.do?method=login"
-  }, [])
+
   return (
     <div>
       Learn React
