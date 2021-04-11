bb9cc9273b20086677960af9548d1097124666c8
dhslrl321 <dhslrl321@gmail.com>
2021-04-11 18:48:15 +0900

    commit

--- Java/JSP/code/src/example/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package example;public class HelloWorld {
-  public String sayHelloWorldFrom(String from) {
-    String result = "Hello, world, from " + from;
-    System.out.println(result);
-    return result;
-  }
-}
--- Spring-Springboot/JPA/JPA-Test-code/src/main/java/com/example/jpatest/model/mm.java
+++
@@ -1,4 +0,0 @@
-package com.example.jpatest.model;
-
-public class mm {
-}
