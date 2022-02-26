63d156695ccb03e97694079bddeb8809d3fe5d91
trevari <trevari@trevariui-MacBookPro.local>
2022-02-26 22:53:07 +0900

    commit

--- js/js 연습/index.js
+++ js/js 연습/index.js
@@ -1,75 +1,41 @@
-const button = document.getElementById("todo-button");
-const input = document.getElementById("todo-form");
-const todoList = document.getElementById("todo-list");
-const connectGetBtn = document.getElementById("connect-get");
-const connectPostBtn = document.getElementById("connect-post")
 
-button.addEventListener("click", (e) => {
-  let todoItem = document.createElement("li");
 
-  todoItem.innerText = input.value;
-  input.value = "";
-  todoList.appendChild(todoItem);
-});
 
-/*connectGetBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  const server = "https://api.github.com/";
-  fetch(server).then((response) => {
-    return response.json();
-  }).then((resData) => {
-
-    const { code_search_url: target } = resData;
-    console.log("Code_Search Url => ", target);
-  }).catch((error) => {
-    console.log("통신 실패");
+const App = () => {
+  const [state ,setState] = useState({
+    user: {
+      username: "",
+      email: ""
+    }
   });
-});*/
 
-connectGetBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  const server = "http://127.0.0.1:8080/api/todo";
-
-  const option = {
-    credentials: "include",
-    method: "GET",
-    headers: {
-      "Accept": "application/json",
-      "Access-Control-Allow-Origin": "*",
-      "Content-Type": "application/json",
-      "mode": "no-cors",
-      "Origin": "*"
-    }
+  const printInfo = () => {
+    const { username, email } = state;
+    console.log(username, email);
   }
-  console.log(option.headers);
+}
 
-  fetch(server, option)
-    .then((response) => {
-      return response.json();
-    }).then((resData) => {
-      console.log(resData);
-    }).catch((error) => {
-      console.log("통신 실패");
-    });
-});
 
-connectPostBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  const server = "http://localhost:8080/api/todo";
-  const option = {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json"
-    }
+
+
+const App = () => {
+  const [state, setState] = useState({
+    user: new User("", "")
+  })
+
+  user.printInfo();
+}
+
+class User {
+  username;
+  email;
+
+  constructor(username, email) {
+    this.username = username;
+    this.email = email;
   }
-  fetch(server, option)
-    .then((response) => {
-      console.log("호출 성공");
-      console.log(response);
-      console.log(response.json);
-    })
-    .catch((error) => {
 
-    });
-
-});
+  printInfo() {
+    console.log(username, email);
+  }
+}
